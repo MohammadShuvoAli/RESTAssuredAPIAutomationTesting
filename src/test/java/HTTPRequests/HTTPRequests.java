2cfa4943f227e3559411eeece9c2070575970efe
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-19 23:31:53 +0600

    POST Request

--- src/test/java/HTTPRequests/HTTPRequests.java
+++ src/test/java/HTTPRequests/HTTPRequests.java
@@ -3,6 +3,9 @@ package HTTPRequests;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 
 public class HTTPRequests {
@@ -22,4 +25,26 @@ public class HTTPRequests {
 			.log().all();
 	}
 	
+	// POST Request		
+			
+	@Test
+	void createUser() {
+		
+		HashMap data = new HashMap();
+		data.put("name","shuvo");
+		data.put("job","SQA Engineer");
+		
+		
+		given()
+			.contentType("application/json")
+			.body(data)
+			
+	    .when()
+	        .post("https://reqres.in/api/users")
+	        
+	    .then()
+	        .statusCode(201)  
+	        .log().all();
+	}
+
 }
