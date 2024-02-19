8322e614577dacf38d124c2e5e475de850d02a1b
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-19 23:02:48 +0600

    First HTTP Request

--- src/test/java/HTTPRequests/HTTPRequests.java
+++ src/test/java/HTTPRequests/HTTPRequests.java
@@ -1,5 +1,24 @@
 package HTTPRequests;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+
 public class HTTPRequests {
+	
+	@Test
+	void getUsers() {
+		
+		given()
+		
+		.when()
+			.get("https://reqres.in/api/users?page=2")
+		
+		.then()
+			.statusCode(200)
+			.body("page", equalTo(2))
+			.log().all();
+	}
 
 }
