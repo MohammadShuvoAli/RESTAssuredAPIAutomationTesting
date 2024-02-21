96bec335afbf7e807cd39ec17564872ca9d5fbe1
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-21 19:39:14 +0600

    WaysToCreatePostRequestBody

---
+++ src/test/java/WaysToCreatePostRequestBody/WaysToCreatePostRequestBody.java
@@ -0,0 +1,21 @@
+package WaysToCreatePostRequestBody;
+
+/*
+Different ways to create POST request body
+1) Post request body using HashMap
+2) Post request body creation using using Org.JSON
+3) Post request body creation using POJO class
+4) Post request using external json file data
+*/
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+public class WaysToCreatePostRequestBody {
+
+}
