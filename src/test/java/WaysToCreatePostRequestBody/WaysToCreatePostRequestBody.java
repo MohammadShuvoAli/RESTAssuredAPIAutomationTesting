fd28354677c2972052ea310ba4b81b6e6fda8cf4
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-22 16:26:23 +0600

    POST Request using HashMap

--- src/test/java/WaysToCreatePostRequestBody/WaysToCreatePostRequestBody.java
+++ src/test/java/WaysToCreatePostRequestBody/WaysToCreatePostRequestBody.java
@@ -1,21 +1,55 @@
 package WaysToCreatePostRequestBody;
 
-/*
-Different ways to create POST request body
-1) Post request body using HashMap
-2) Post request body creation using using Org.JSON
-3) Post request body creation using POJO class
-4) Post request using external json file data
-*/
-
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
-
 import java.util.HashMap;
-
 import org.testng.annotations.Test;
 
 public class WaysToCreatePostRequestBody {
 
+    String id;
+
+    @Test(priority = 1)
+    void testPostRequestUsingHashMap() {
+
+        HashMap data = new HashMap();
+        data.put("name", "Shuvo");
+        data.put("location", "BD");
+        data.put("phone", "77777777777");
+
+        String courseArr[] = {"C", "C++"};
+        data.put("courses", courseArr);
+
+        id = given()
+            .contentType("application/json")
+            .body(data)
+        .when()
+            .post("http://localhost:3000/students")
+            .jsonPath().getString("id");
+
+        given()
+            
+        .when()
+            .get("http://localhost:3000/students/" + id)
+        .then()
+            .statusCode(200)
+            .body("name", equalTo("Shuvo"))
+            .body("location", equalTo("BD"))
+            .body("phone", equalTo("77777777777"))
+            .body("courses[0]", equalTo("C"))
+            .body("courses[1]", equalTo("C++"))
+            .log().all();
+    }
+
+    @Test(priority = 2)
+    void testDelete() {
+        // Use the stored ID from the POST request in the DELETE request
+        given()
+        
+        .when()
+            .delete("http://localhost:3000/students/" + id)
+        .then()
+            .statusCode(200)
+            .log().all();
+    }
 }
