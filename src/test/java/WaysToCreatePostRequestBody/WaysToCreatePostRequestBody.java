fafd9b0a3ad93531322e4b2ff63317d2690d3503
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-22 22:36:20 +0600

    POST Request using JSON Library

--- src/test/java/WaysToCreatePostRequestBody/WaysToCreatePostRequestBody.java
+++ src/test/java/WaysToCreatePostRequestBody/WaysToCreatePostRequestBody.java
@@ -3,13 +3,15 @@ package WaysToCreatePostRequestBody;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
+
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 public class WaysToCreatePostRequestBody {
 
     String id;
 
-    @Test(priority = 1)
+    //@Test(priority = 1)
     void testPostRequestUsingHashMap() {
 
         HashMap data = new HashMap();
@@ -41,6 +43,40 @@ public class WaysToCreatePostRequestBody {
             .log().all();
     }
 
+    @Test(priority = 1)
+    void testPostRequestUsingJsonLibrary() {
+
+        JSONObject data = new JSONObject();
+        
+        data.put("name", "Shuvo");
+        data.put("location", "BD");
+        data.put("phone", "77777777777");
+
+        String courseArr[] = {"C", "C++"};
+        data.put("courses", courseArr);
+
+        id = given()
+            .contentType("application/json")
+            .body(data.toString()) // getting as JSON so need to convert into string
+        .when()
+            .post("http://localhost:3000/students")
+            .jsonPath().getString("id");
+
+        given()
+            
+        .when()
+            .get("http://localhost:3000/students/" + id)
+        .then()
+            .statusCode(200)
+            .body("name", equalTo("Shuvo"))
+            .body("location", equalTo("BD"))
+            .body("phone", equalTo("77777777777"))
+            .body("courses[0]", equalTo("C"))
+            .body("courses[1]", equalTo("C++"))
+            .log().all();
+    }
+
+    
     @Test(priority = 2)
     void testDelete() {
         // Use the stored ID from the POST request in the DELETE request
