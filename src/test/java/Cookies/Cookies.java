e5aa5031ec7d4b9ae53e0173e0215bf12686242f
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-25 23:07:33 +0600

    Create Cookies.java

---
+++ src/test/java/Cookies/Cookies.java
@@ -0,0 +1,16 @@
+package Cookies;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+public class Cookies {
+	
+	@Test
+	void testCookie() {
+		
+	}
+
+}
