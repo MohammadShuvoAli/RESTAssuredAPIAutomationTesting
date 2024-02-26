cd0b12311425f3e2a54358840dead48dd10e7a07
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-26 22:19:35 +0600

    get cookies info

--- src/test/java/Cookies/Cookies.java
+++ src/test/java/Cookies/Cookies.java
@@ -1,25 +1,40 @@
 package Cookies;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-public class Cookies {
-	
-	@Test
-	void testCookie() {
-		
-		given()
-		
-		.when()
-			.get("https://www.google.com/")
-			
-		.then()
-			.statusCode(200)
-            .cookie("AEC", not(equalTo("Ae3NU9Ne-UmWjsSi-jgYvIjiRiRR-sv4uYydAUnANevGqGVSFvVjIlaJcw")))
-			.log().all();		
-	}
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.equalTo;
 
+public class Cookies {
+
+    @Test(priority=1)
+    void testCookie() {
+        given()
+        .when()
+            .get("https://www.google.com/")
+        .then()
+            .statusCode(200)
+            .cookie("AEC", not(equalTo("Ae3NU9Ne-UmWjsSi-jgYvIjiRiRR-sv4uYydAUnANevGqGVSFvVjIlaJcw")))
+            .log().all();
+    }
+
+    @Test(priority=2)
+    void getCookiesInfo() {
+        Response response = given()
+        .when()
+            .get("https://www.google.com/");
+
+        // Get single cookie info
+        String cookieValue = response.getCookie("AEC");
+        System.out.println("Value of cookie is====>" + cookieValue);
+
+        // Get all cookies info
+        response.getCookies().forEach((name, value) -> {
+            System.out.println("Cookie Name: " + name);
+            System.out.println("Cookie Value: " + value);
+            System.out.println("------------------------------");
+        });
+    }
 }
