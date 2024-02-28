5b1159df11d68575a590627788889cc66f016d79
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-02-28 15:06:39 +0600

    test headers

--- src/test/java/Headers/Headers.java
+++ src/test/java/Headers/Headers.java
@@ -10,11 +10,17 @@ import static org.hamcrest.Matchers.equalTo;
 public class Headers {
 	
 	@Test(priority=1)
-    void testHeader() {
+    void testHeaders() {
         given()
         .when()
             .get("https://www.google.com/")
         .then()
+        	.header("Content-Type", "text/html; charset=ISO-8859-1")
+        	.and() // for adding multiple validation
+        	.header("Content-Encoding", "gzip")
+        	.and()
+        	.header("Server", "gws")
+        	.and()
             .statusCode(200)
             .log().all();
     }
