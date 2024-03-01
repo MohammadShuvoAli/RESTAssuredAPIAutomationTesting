bfb6d8953433bd203fad6f60d60c2f072a13fea0
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-01 21:46:51 +0600

    get Headers

---
+++ src/test/java/Headers/HeadersTest.java
@@ -0,0 +1,40 @@
+package Headers;
+
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class HeadersTest {
+
+    @Test(priority=1)
+    void testHeaders() {
+        given()
+        .when()
+            .get("https://www.google.com/")
+        .then()
+            .header("Content-Type", "text/html; charset=ISO-8859-1")
+            .and()
+            .header("Content-Encoding", "gzip")
+            .and()
+            .header("Server", "gws")
+            .and()
+            .statusCode(200)
+            .log().all();
+    }
+
+    @Test(priority=2)
+    void getHeaders() {
+        Response res = given()
+        .when()
+            .get("https://www.google.com/");
+
+        Headers headers = res.getHeaders();
+
+        for (Header header : headers.asList()) {
+            System.out.println(header.getName() + "       " + header.getValue());
+        }
+    }
+}
