3c9ef6413614efad252273d50c79eb0dff4d9dbd
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-11 21:30:16 +0600

    Update ParsingJSONResponseData.java

--- src/test/java/ParsingJSONResponseData/ParsingJSONResponseData.java
+++ src/test/java/ParsingJSONResponseData/ParsingJSONResponseData.java
@@ -1,5 +1,22 @@
 package ParsingJSONResponseData;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
 public class ParsingJSONResponseData {
 
+	@Test(priority=1)
+	void testJsonResponse() {
+		
+		given()
+		
+		.when()
+		
+		.then()
+		;
+	}
+	
 }
