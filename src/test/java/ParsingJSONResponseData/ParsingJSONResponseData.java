0afd13c585876f504beb69348916d8a266b2592b
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-13 04:43:41 +0600

    Test JSON Response using Assert

--- src/test/java/ParsingJSONResponseData/ParsingJSONResponseData.java
+++ src/test/java/ParsingJSONResponseData/ParsingJSONResponseData.java
@@ -1,14 +1,15 @@
 package ParsingJSONResponseData;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
 
 public class ParsingJSONResponseData {
   
-
 	@Test(priority=1)
 	void testJsonResponse() {
 		
@@ -22,6 +23,23 @@ public class ParsingJSONResponseData {
 			.statusCode(200)
 			.header("Content-Type", "application/json")
 			.body("book[2].title", equalTo("To Kill a Mockingbird"));
+		
+	}
+	
+	@Test(priority=2)
+	void testJsonResponseAssert() {
+		
+		Response res = given()
+				.contentType("ContentType.JSON")
+				
+			.when()
+				.get("http://localhost:3000/store");
+		
+		Assert.assertEquals(res.getStatusCode(), 200); // validation 1	
+		Assert.assertEquals(res.header("Content-Type"), "application/json"); // validation 2
+		
+		String bookname = res.jsonPath().get("book[2].title").toString();
+		Assert.assertEquals(bookname, "To Kill a Mockingbird");
 		
 	}
 	
