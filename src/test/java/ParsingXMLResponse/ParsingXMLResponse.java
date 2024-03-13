fa0719d7e975c3607908fa0b34e74505afc60622
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-13 18:12:41 +0600

    XML Response Data Validation

--- src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
+++ src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
@@ -38,6 +38,37 @@ public class ParsingXMLResponse {
 		
 		Assert.assertEquals(res.statusCode(), 200);
 		Assert.assertEquals(res.header("Content-Type"), "application/xml; charset=utf-8");
+		
+		String cityName = res.xmlPath().get("root.city").toString();
+		String firstName = res.xmlPath().get("root.firstName").toString();
+		String lastName = res.xmlPath().get("root.lastName").toString();
+		String stateName = res.xmlPath().get("root.state").toString();
+		
+		Assert.assertEquals(cityName, "San Jose");
+		Assert.assertEquals(firstName, "John");
+		Assert.assertEquals(lastName, "Doe");
+		Assert.assertEquals(stateName, "CA");
+	}
+	
+	@Test(priority=3)
+	void testXMLResponseDataValidation() {
+		
+		Response res = 
+		
+		given()
+		
+		.when()
+			.get("https://mocktarget.apigee.net/xml");
+		
+		String cityName = res.xmlPath().get("root.city").toString();
+		String firstName = res.xmlPath().get("root.firstName").toString();
+		String lastName = res.xmlPath().get("root.lastName").toString();
+		String stateName = res.xmlPath().get("root.state").toString();
+		
+		Assert.assertEquals(cityName, "San Jose");
+		Assert.assertEquals(firstName, "John");
+		Assert.assertEquals(lastName, "Doe");
+		Assert.assertEquals(stateName, "CA");
 	}
 
 }
