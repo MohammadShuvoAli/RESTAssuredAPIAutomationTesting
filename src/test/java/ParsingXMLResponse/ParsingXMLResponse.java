b528088e4b3f198ba106c9cf9957880f42899adc
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-13 18:02:44 +0600

    Test XML Response

--- src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
+++ src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
@@ -4,7 +4,10 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
 
 public class ParsingXMLResponse {
 	
@@ -14,11 +17,26 @@ public class ParsingXMLResponse {
 		given()
 		
 		.when()
+			.get("https://mocktarget.apigee.net/xml")
 		
 		.then()
+			.statusCode(200)
+			.header("Content-Type", "application/xml; charset=utf-8")
+			.log().all();
 		
-		;
+	}
+	
+	@Test(priority=2)
+	void testXMLResponseAssert() {
 		
+		Response res = 
+		
+		given()
+		
+		.when()
+			.get("https://mocktarget.apigee.net/xml");
+		
+		Assert.assertEquals(res.header("Content-Type"), "application/xml; charset=utf-8");
 	}
 
 }
