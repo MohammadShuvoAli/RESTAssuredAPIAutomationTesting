d21dd0c78ba9b9205eb0977c205c1e5dde03ce61
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-13 18:12:57 +0600

    Update ParsingXMLResponse.java

--- src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
+++ src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
@@ -39,15 +39,6 @@ public class ParsingXMLResponse {
 		Assert.assertEquals(res.statusCode(), 200);
 		Assert.assertEquals(res.header("Content-Type"), "application/xml; charset=utf-8");
 		
-		String cityName = res.xmlPath().get("root.city").toString();
-		String firstName = res.xmlPath().get("root.firstName").toString();
-		String lastName = res.xmlPath().get("root.lastName").toString();
-		String stateName = res.xmlPath().get("root.state").toString();
-		
-		Assert.assertEquals(cityName, "San Jose");
-		Assert.assertEquals(firstName, "John");
-		Assert.assertEquals(lastName, "Doe");
-		Assert.assertEquals(stateName, "CA");
 	}
 	
 	@Test(priority=3)
