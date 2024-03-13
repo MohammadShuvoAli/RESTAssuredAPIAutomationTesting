0dd066c38c6eeec23302afda0af2cd9b0c7c5398
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-13 15:22:33 +0600

    Parsing XML Response

---
+++ src/test/java/ParsingXMLResponse/ParsingXMLResponse.java
@@ -0,0 +1,24 @@
+package ParsingXMLResponse;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+public class ParsingXMLResponse {
+	
+	@Test(priority=1)
+	void testXMLResponse() {
+		
+		given()
+		
+		.when()
+		
+		.then()
+		
+		;
+		
+	}
+
+}
