61e66a35bc4a4a67612f224d321a5197d9e504f8
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-15 23:26:53 +0600

    Multiple File Upload

--- src/test/java/FileUploadAndDownload/FileUploadAndDownload.java
+++ src/test/java/FileUploadAndDownload/FileUploadAndDownload.java
@@ -9,14 +9,14 @@ import org.testng.annotations.Test;
 
 public class FileUploadAndDownload {
 	
-	@Test(priority=1)
+	//@Test(priority=1)
 	void SingleFileUpload() {
 		// Define the file to upload
-		File myfile = new File(System.getProperty("user.dir") + "\\src\\test\\java\\FileUploadAndDownload\\myfile.txt");
+		File myfile1 = new File(System.getProperty("user.dir") + "\\src\\test\\java\\FileUploadAndDownload\\myfile1.txt");
 
 		// Perform file upload
 		given()
-		    .multiPart("file", myfile)
+		    .multiPart("file", myfile1)
 		    .contentType("multipart/form-data")
 		    
 		.when()
@@ -24,8 +24,32 @@ public class FileUploadAndDownload {
 		    
 		.then()
 		    .statusCode(200) // Assert that the status code is 200 OK
-		    .body("fileName", equalTo("myfile.txt")) // Assert response body contains fileName
+		    .body("fileName", equalTo("myfile1.txt")) // Assert response body contains fileName
 		    .log().all(); // Log all response details
 		
 	}
+	
+	@Test(priority=1)
+	void MultipleFileUpload() {
+		// Define the file to upload
+		File myfile1 = new File(System.getProperty("user.dir") + "\\src\\test\\java\\FileUploadAndDownload\\myfile1.txt");
+		File myfile2 = new File(System.getProperty("user.dir") + "\\src\\test\\java\\FileUploadAndDownload\\myfile2.txt");
+
+		// Perform file upload
+		given()
+		    .multiPart("file", myfile1)
+		    .multiPart("file", myfile2)
+		    .contentType("multipart/form-data")
+		    
+		.when()
+		    .post("http://localhost:8080/uploadMultipleFiles")
+		    
+		.then()
+		    .statusCode(200) // Assert that the status code is 200 OK
+		    .body("[0].fileName", equalTo("myfile1.txt")) 
+		    .body("[1].fileName", equalTo("myfile2.txt")) 
+		    .log().all(); // Log all response details
+		
+	}
+	
 }
