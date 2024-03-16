9c90db869929c51172deac7383593b48b998ea0b
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-03-16 21:49:29 +0600

    Json Schema Validation

---
+++ src/test/java/JsonSchemaValidation/JsonSchemaValidation.java
@@ -0,0 +1,23 @@
+package JsonSchemaValidation;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+public class JsonSchemaValidation {
+	
+	@Test(priority=1)
+	void testJsonSchemaValidation() {
+		
+		given()
+		
+		.when()
+		
+		.then()
+		
+		;
+		
+	}
+
+}
