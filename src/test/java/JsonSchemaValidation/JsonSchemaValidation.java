9d803e6b0a5334b45829f6956572586deb40deb7
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-04-02 22:23:56 +0600

    update

--- src/test/java/JsonSchemaValidation/JsonSchemaValidation.java
+++ src/test/java/JsonSchemaValidation/JsonSchemaValidation.java
@@ -1,33 +1,20 @@
 package JsonSchemaValidation;
 
-import io.restassured.response.Response;
-import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.equalTo;
-
-import org.testng.annotations.Test;
-
 import io.restassured.module.jsv.JsonSchemaValidator;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 
 public class JsonSchemaValidation {
-	
-	String filePath = System.getProperty("user.dir") + "\\src\\test\\resources\\StoreAPISchema.json";
-	
-	@Test(priority=1)
-	void testJsonSchemaValidation() {
-		
-		given()
-		
-		.when()
-			.get("http://localhost:3000/store")
-		
-		.then()
-			.assertThat().body(JsonSchemaValidator.matchesJsonSchema(filePath)
-		
-		;
-		
-	}
+    
+    String filePath = System.getProperty("user.dir") + "\\src\\test\\resources\\StoreAPISchema.json";
 
+    @Test(priority=1)
+    void testJsonSchemaValidation() {
+        System.out.println("File path: " + filePath); // Print the file path
+        given()
+        .when()
+            .get("http://localhost:3000/store")
+        .then()
+            .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(filePath));
+    }
 }
